72d818dd38c740b80fde348a295146d92c6c2477
Rinat A <rinat.a@ezscratch.com>
2018-03-13 20:40:16 +0500

    Added auto-generating data for model in 'common' app

--- rest/apps/common/models.py
+++ rest/apps/common/models.py
@@ -1,3 +1,4 @@
+from itertools import islice
 from django.db import models
 
 
@@ -10,6 +11,18 @@ class Organization(models.Model):
 
     def __str__(self):
         return self.name
+
+    @staticmethod
+    def _bootstrap(count=500, locale='ru', batch_size=100):
+        from mimesis import Business
+        business = Business(locale)
+        Organization.objects.all().delete()
+        objs = (Organization(name=business.company()) for _ in range(count))
+        while True:
+            batch = list(islice(objs, batch_size))
+            if not batch:
+                break
+            Organization.objects.bulk_create(batch, batch_size)
 
 
 class Offer(models.Model):
@@ -39,3 +52,39 @@ class Offer(models.Model):
 
     def __str__(self):
         return self.name
+
+    @staticmethod
+    def _bootstrap(count=10000, locale='ru', batch_size=100):
+        from random import randint, choice
+        from mimesis import Text, Datetime
+        from pytz import UTC
+        text = Text(locale)
+        datetime = Datetime(locale)
+        typ_choice = [Offer.CONSUMER, Offer.MORTGAGE, Offer.AUTO, Offer.CSMB]
+
+        def rand_dt():
+            _dt = datetime.datetime(start=2016, end=2018)
+            return _dt.replace(tzinfo=UTC)
+
+        def rand_txt(quantity=10):
+            return ' '.join(text.words(quantity=quantity))
+
+        Offer.objects.all().delete()
+        if not Organization.objects.exists():
+            Organization._bootstrap()
+        organizations_id = Organization.objects.values_list('pk', flat=True)
+        offers_gen = (
+            Offer(rotation_start=rand_dt(),
+                  rotation_finish=rand_dt(),
+                  name=rand_txt(),
+                  typ=choice(typ_choice),
+                  min_score=randint(0, 1000),
+                  max_score=randint(500, 2000),
+                  organization_id=choice(organizations_id))
+            for _ in range(count)
+        )
+        while True:
+            batch = list(islice(offers_gen, batch_size))
+            if not batch:
+                break
+            Offer.objects.bulk_create(batch, batch_size)

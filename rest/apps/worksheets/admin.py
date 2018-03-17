d55fd674ed06264e0706170d8078fb3e9be7ad9e
Rinat A <rinat.a@ezscratch.com>
2018-03-17 17:34:54 +0500

    List filter for 'Worksheets' by 'partner'

--- rest/apps/worksheets/admin.py
+++ rest/apps/worksheets/admin.py
@@ -1,13 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from apps.common.constants import GROUP_PARTNERS
 from . import models as m
-# Register your models here.
+
+
+class PartnerListFilter(admin.SimpleListFilter):
+    title = 'Партнёр'
+    parameter_name = 'partner'
+
+    def lookups(self, request, model_admin):
+        partner_users = User.objects\
+            .filter(groups__name=GROUP_PARTNERS)\
+            .values_list('pk', 'username')
+        return tuple(partner_users)
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(owner_id=self.value())
+        return queryset
 
 
 @admin.register(m.Worksheet)
 class WorksheetAdmin(admin.ModelAdmin):
     list_display = ('created', 'surname', 'first_name', 'patronymic',
-                    'phone_num', 'passport')
+                    'phone_num', 'passport', 'owner')
     list_display_links = ('surname', 'first_name', 'patronymic', )
+    list_filter = (PartnerListFilter, )
 
     def passport(self, obj):
         return '{} {}'.format(obj.pass_ser, obj.pass_num)
